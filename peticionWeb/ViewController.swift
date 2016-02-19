de04748e3c8cb10bed8418650853dc7b3f6b479c
Adrian Camacho Soriano <acaso@macbook-air-de-adrian.local>
2016-02-19 11:44:47 -0500

    Initial Commit

---
+++ peticionWeb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  peticionWeb
+//
+//  Created by Adrian Camacho Soriano on 19/02/16.
+//  Copyright © 2016 Adrian Camacho Soriano acaso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ peticionWebTests/peticionWebTests.swift
@@ -0,0 +1,36 @@
+//
+//  peticionWebTests.swift
+//  peticionWebTests
+//
+//  Created by Adrian Camacho Soriano on 19/02/16.
+//  Copyright © 2016 Adrian Camacho Soriano acaso. All rights reserved.
+//
+
+import XCTest
+@testable import peticionWeb
+
+class peticionWebTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
